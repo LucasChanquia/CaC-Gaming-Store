f67ab5ab95dba5e5047fd3566a22cdaaa08d48ac
LucasChanquia <lucaschanquia@gmail.com>
2024-06-01 21:24:05 -0300

    fix: :bug: cambio URL
    configuración de rutas

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
       // Redirigir si ambos campos son válidos
       if (isValid) {
-        window.location.href = '/GamingStore-java.github.io/pages/index.html';
+        window.location.href = '/CaC-Gaming-Store/pages/index.html';
       }
     });
   
